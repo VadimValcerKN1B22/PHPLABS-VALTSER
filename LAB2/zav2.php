85b9fe77a0217034a295bf0e45d947853256f363
VadimValcerKN1B22 <159442708+VadimValcerKN1B22@users.noreply.github.com>
2025-03-11 12:45:43 +0200

    Add files via upload

---
+++ LAB2/zav2.php
@@ -0,0 +1,9 @@
+<?php
+$strNumber = "3.14";
+$intNumber = (int)$strNumber;
+$floatNumber = (float)$strNumber;
+
+echo "Рядкове значення: $strNumber <br>";
+echo "Ціле число: $intNumber <br>";
+echo "Число з плаваючою комою: $floatNumber <br>";
+?>
